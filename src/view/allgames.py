d8ed29f87487f076bf4ee5c879512ad5db29a7aa
thatcrazygame <thatcrazygame@gmail.com>
2024-11-07 16:52:02 -0500

    Make All Games View more compact to fit 6 games at a time

--- src/view/allgames.py
+++ src/view/allgames.py
@@ -4,11 +4,13 @@ from time import perf_counter
 from PIL import Image, ImageColor, ImageDraw
 
 from constants import (
-    ABBRPADDING,
     ALLGAMES,
+    ALIGN_CENTER,
     BG,
     LEAGUEDEFAULT,
     LEAGUE_COLORS,
+    NOT_FOUND,
+    PANEL_HEIGHT,
     PANEL_WIDTH,
     ROTATETIME,
 )
@@ -33,15 +35,16 @@ class AllGames(View):
         self.last_rotate = perf_counter()
         self.league = LEAGUEDEFAULT
 
-        self.states_scroll = ScrollingText(
+        self.filter_scroll = ScrollingText(
             font=FONT_5X8,
             color=WHITE,
-            starting_x=ABBRPADDING,
-            y=(2 * FONT_5X8.height) + ABBRPADDING,
-            left_bound=ABBRPADDING,
-            right_bound=PANEL_WIDTH,
-            num_spaces=1,
+            starting_x=2,
+            y=PANEL_HEIGHT - 3,
+            left_bound=2,
+            right_bound=PANEL_WIDTH * 2,
             speed=0.02,
+            align=ALIGN_CENTER,
+            num_spaces=3,
         )
 
     def get_colors(self, team_colors: list[str]):
@@ -55,7 +58,7 @@ class AllGames(View):
 
     def draw_team(self, canvas, x, y, abbr, score, colors):
         bg, txt, outline = self.get_colors(colors)
-        row_height = FONT_4X6.height + (2 * ABBRPADDING)
+        row_height = FONT_4X6.height + 3
         top = 0
         left = 0
         bottom = row_height
@@ -78,11 +81,11 @@ class AllGames(View):
 
         canvas.SetImage(img, x, y)
 
-        y = y + FONT_4X6.height + ABBRPADDING
+        y = y + FONT_4X6.height + 1
         DrawText(
             canvas,
             FONT_4X6,
-            x + ABBRPADDING,
+            x + 2,
             y,
             ColorRGB(*txt),
             abbr,
@@ -90,22 +93,22 @@ class AllGames(View):
         DrawText(
             canvas,
             FONT_4X6,
-            x + ABBRPADDING + middle,
+            x + 2 + middle,
             y,
             WHITE,
             f"{score}",
         )
 
     def draw_game(self, canvas, x, y, game):
-        row_height = FONT_4X6.height + (2 * ABBRPADDING)
+        row_height = 9
         top = 0
         left = 0
-        bottom = row_height * 2
+        bottom = row_height * 2 - 1
         right = PANEL_WIDTH
 
         team_width = int(PANEL_WIDTH / 2)
 
-        background = Image.new("RGB", (PANEL_WIDTH, row_height * 2), BLACK.rgb)
+        background = Image.new("RGB", (PANEL_WIDTH, bottom), BLACK.rgb)
         background_draw = ImageDraw.Draw(background)
 
         league_color = WHITE
@@ -141,11 +144,11 @@ class AllGames(View):
         if "delayed" in clock.lower():
             clock = "Delayed"
 
-        clock_x = x + ABBRPADDING
-        clock_y = y + FONT_4X6.height + ABBRPADDING
+        clock_x = x + 2
+        clock_y = y + FONT_4X6.height + 1
         DrawText(canvas, FONT_4X6, clock_x, clock_y, WHITE, clock)
 
-        y += row_height
+        y += row_height - 1
         self.draw_team(
             canvas,
             x,
@@ -168,7 +171,7 @@ class AllGames(View):
         return bottom
 
     async def draw(self, canvas, data: Data):
-        num_games = 5
+        num_games = 6
 
         offset = self.offset
         league_filter = data.all_games["league_filter"]
@@ -188,23 +191,22 @@ class AllGames(View):
             self.last_rotate = perf_counter()
         self.league = league_filter
 
-        league_x = ABBRPADDING
-        league_y = FONT_5X8.height
-        DrawText(canvas, FONT_5X8, league_x, league_y, WHITE, self.league)
+        x = 0
+        y = 0
+        for i, game in enumerate(games[:num_games]):
+            if i == 3:
+                x = PANEL_WIDTH
+                y = 0
+            y += self.draw_game(canvas, x, y, game)
 
-        all_states = f"No games for league: {self.league} state: {state_filter}  "
+        filter_txt = f"No games for league: {self.league} state: {state_filter}"
+        all_states = ""
         if games:
             states = Counter([game["state"] for game in games])
             all_states = " ".join(
                 [f"{state}: {count}" for state, count in sorted(states.items())]
             )
+            all_states = all_states.replace(NOT_FOUND, "No Game")
+            filter_txt = f"{self.league} - {all_states}"
 
-        self.states_scroll.draw(canvas, all_states)
-
-        x = 0
-        y = 21
-        for i, game in enumerate(games[:num_games]):
-            if i == 2:
-                x = PANEL_WIDTH
-                y = 1
-            y += self.draw_game(canvas, x, y, game)
+        self.filter_scroll.draw(canvas, filter_txt)
